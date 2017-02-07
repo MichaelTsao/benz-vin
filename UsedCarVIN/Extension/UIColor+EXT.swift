93f3bec52076e4772bb3e39f6c470beaba73bc88
于飞 <yufei@dakashuo.com>
2017-02-07 20:05:31 +0800

    2017年02月07日20:05:22 --

--- UsedCarVIN/Extension/UIColor+EXT.swift
+++ UsedCarVIN/Extension/UIColor+EXT.swift
@@ -23,6 +23,33 @@ extension UIColor {
         }}
     
     
+//    164	176	190	
+    open class var label_tip_color: UIColor { get{
+        return UIColor.init("#A4B0BE")
+        }}
+    
+    
+    //    0 0 0
+    open class var label_text_color: UIColor { get{
+        return UIColor.init("#000000")
+        }}
+    
+    //
+    open class var title_label_text_color: UIColor { get{
+        return UIColor.init("#6C6C6C")
+        }}
+    
+    //    蓝色 41	144	216
+    open class var circular_label_text_color: UIColor { get{
+        return UIColor.init("#2990D8")
+        }}
+    
+    //    深灰色
+    open class var night_background_color: UIColor { get{
+        return UIColor.init("#2A2F36")
+        }}
+    
+    
     open class var nav_black: UIColor { get{
         return UIColor.init(red: 36.0/255.0, green: 36.0/255.0, blue: 36.0/255.0, alpha: 1.0)
     }}
---
+++ UsedCarVIN/Search/View/infoMaintainView.swift
@@ -0,0 +1,10 @@
+
+//
+//  infoMaintainView.swift
+//  UsedCarVIN
+//
+//  Created by DreamTeamAAn on 2017/2/7.
+//  Copyright © 2017年 daimler. All rights reserved.
+//
+
+import Foundation
