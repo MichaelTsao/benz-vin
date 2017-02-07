771061c4709e46f07ca78db44fc72e880a35351b
Michael Tsao <caoxiang@yeah.net>
2017-02-07 20:40:04 +0800

    change report view to detail view

--- UsedCarVIN/Report/Controller/ReportsViewController.swift
+++ UsedCarVIN/Report/Controller/ReportsViewController.swift
@@ -9,96 +9,184 @@
 import UIKit
 
 class ReportsViewController: BaseViewController {
-
-
-    var dataArray:[VehicleModel] = []
-
+    
+    
+    var dataArray:[MaintenanceModel] = []
+    
     let tableView:UITableView = {
         let tableView = UITableView(frame: CGRect.zero, style: .plain)
         tableView.separatorStyle = .none
         tableView.backgroundColor = UIColor.table_background
         return tableView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.title = "我的报告"
+        
+        self.title = "维护记录"
         self.view.backgroundColor = UIColor.table_background
-
+        
+        let vin = "4JG1641861A355196";
+        let path = "http://120.77.66.101:8888/Vehicle/History?VIN=\(vin)"
+        
+        print("path->:"+path);
+        
+        XBNetHandle.getRequestWithUrlStr(urlStr: path, successBlock: { (result) in
+            let str = String(data:result!, encoding:String.Encoding.utf8)
+            print("Json Str:");
+            print(str ?? "")
+            let json : AnyObject! = try? JSONSerialization
+                .jsonObject(with: result!, options:JSONSerialization.ReadingOptions.allowFragments) as AnyObject!
+            print("Json Object:"); print(json)
+            
+            let number :Int = json?["resultCode"] as! Int
+            
+            if (number == 0)
+            {
+                print("resultCode->:");
+                print(json)
+                
+                let dataDic = json?["data"] as? NSArray
+                
+                print("resultCode-dataDic>:");
+                print(dataDic ?? "")
+                
+                //                dataArray = []
+                
+                self.tableView.isHidden = false
+                
+                //                let names = ["hell0","world","let's","start"]
+                for var modelDic in dataDic! {
+                    let model = MaintenanceModel()
+                    model.type       = "维修"
+                    model.kilometres = "40027公里"
+                    model.date       = "2016-07-01"
+                    model.component  = "空调"
+                    model.content    = "更换氟管密封圈"
+                    
+                    model.regNo = (modelDic as AnyObject).object(forKey: "regNo") as! String?
+                    model.maintainDate = (modelDic as AnyObject).object(forKey: "maintainDate") as! String?
+                    model.details = (modelDic as AnyObject).object(forKey: "details") as! String?
+                    model.desc = (modelDic as AnyObject).object(forKey: "desc") as! String?
+                    //                    model.miles    = String(describing:(modelDic as AnyObject).object(forKey: "miles"))
+                    
+                    if let number = (modelDic as AnyObject).object(forKey: "miles") { // 建议的做法
+                        model.miles = String(describing:number)+"公里"
+                    }else{
+                        model.miles = ""
+                    }
+                    
+                    if let sanName = (modelDic as AnyObject).object(forKey: "sanName") {
+                        model.sanName = String(describing:sanName)
+                    }else{
+                        model.sanName = "SA1"
+                    }
+                    if model.sanName == "<null>" {
+                        model.sanName = "SA1"
+                    }
+                    
+                    model.vin    = (modelDic as AnyObject).object(forKey: "vin") as! String?
+                    self.dataArray.append(model)
+                    print(modelDic)
+                }
+                
+                self.tableView.isHidden = false
+                
+                self.tableView.reloadData()
+                
+                
+            }else{
+                if let number = json?["message"] as? String {
+                    print(number)
+                    HUD.showErrorMsg(message: number)
+                }
+                
+            }
+            //                HUD.hideWaiting()
+        }) { (Error) in
+            //            code
+            print(Error ?? "")
+            //                HUD.hideWaiting()
+            HUD.showErrorMsg(message: "请求服务器出错")
+        }
+        
         HUD.showWaiting()
         //加载Mocks数据
         DispatchQueue.global(qos: .default).async {
-            let array = [Mocks().vehicle1,Mocks().vehicle2,Mocks().vehicle3]
+            //            let array = [Mocks().vehicle1,Mocks().vehicle2,Mocks().vehicle3]
             DispatchQueue.main.async {
                 HUD.hideWaiting()
-                self.dataArray = array
+                //                self.dataArray = array
                 self.tableView.reloadData()
             }
         }
     }
-
+    
     override func commonInit() {
         self.tableView.delegate   = self
         self.tableView.dataSource = self
-
+        
         self.view.addSubview(tableView)
         self.view.setNeedsUpdateConstraints()
     }
-
+    
     override func updateViewConstraints() {
         if (!didSetupConstraints) {
-
+            
             tableView.snp.makeConstraints({ (make) in
                 make.edges.equalTo(self.view)
             })
-
+            
             didSetupConstraints = true
         }
-
+        
         super.updateViewConstraints()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
 extension ReportsViewController:UITableViewDelegate,UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         return self.dataArray.count
-
+        
     }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-
+        
         return ReportTableViewCell.cellHeight()
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let identifierString = ReportTableViewCell.reuseIdentifierString!
-
+        
         var cell:ReportTableViewCell? = tableView.dequeueReusableCell(withIdentifier: identifierString) as? ReportTableViewCell
-
+        
         if cell == nil {
             cell = ReportTableViewCell(style: .default, reuseIdentifier: identifierString)
         }
-
+        
         let vehicle = self.dataArray[indexPath.row]
-
-        cell!.titleLabel.text   = vehicle.licensePlate
-        cell!.dateLabel.text    = vehicle.bornYear
-        cell!.contentLabel.text = vehicle.title
-        cell!.vinLabel.value    = vehicle.vin
-
+        
+        cell!.descLabel.text   = vehicle.desc
+        cell!.dateLabel.text    = vehicle.maintainDate
+        cell!.mileLabel.text    = vehicle.miles
+//        cell!.contentLabel.text = vehicle.regNo
+        cell!.sanLabel.value    = vehicle.sanName
+        cell!.typeLabel.text = vehicle.details
+        
         return cell!
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        let resultsViewController   = SearchResultsViewController()
-        resultsViewController.model = self.dataArray[indexPath.row]
-        self.navigationController?.pushViewController(resultsViewController, animated: true)
+        
+//        let resultsViewController   = SearchResultsViewController()
+//        resultsViewController.model = self.dataArray[indexPath.row]
+//        self.navigationController?.pushViewController(resultsViewController, animated: true)
     }
 }
--- UsedCarVIN/Report/View/ReportTableViewCell.swift
+++ UsedCarVIN/Report/View/ReportTableViewCell.swift
@@ -8,97 +8,201 @@
 
 import UIKit
 
+class BGView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override func draw(_ rect: CGRect) {
+//        let h = rect.height
+//        let w = rect.width
+        let color:UIColor = UIColor(red: 55.00/255, green: 62.00/255, blue: 70.00/255, alpha: 1.00)
+        
+//        let drect = CGRect(x: (w * 0.25),y: (h * 0.25),width: (w * 0.5),height: (h * 0.5))
+        let bpath:UIBezierPath = UIBezierPath(rect: rect)
+        color.setFill()
+        bpath.fill()
+        
+        color.set()
+        bpath.stroke()
+        
+        NSLog("drawRect has updated the view")
+    }
+}
+
 class ReportTableViewCell: BaseTableViewCell,UITableViewCellHeightProtocol {
 
-    let bgView:UIImageView = {
-
-        let image     = UIImage(named: "background_report")
-        let imageView = UIImageView(image: image)
-        return imageView
-    }()
-
-    let titleLabel:UILabel = {
+    var bgView:UIView = BGView(frame: CGRect(
+        origin: CGPoint(x: 10, y: 10),
+        size: CGSize(width: 340, height: 220)))
+    
+    let descLabel:UILabel = {
 
         let label = UILabel()
-        label.textColor = UIColor.black
+        label.textColor = UIColor.white
         label.font      = UIFont.systemFont(ofSize: 14)
+        label.sizeToFit()
         return label
     }()
 
     let dateLabel:UILabel = {
 
         let label = UILabel()
-        label.textColor     = UIColor.lightGray
-        label.font          = UIFont.systemFont(ofSize: 14)
-        label.textAlignment = .right
+        label.textColor     = UIColor.white
+        label.font          = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .left
         return label
     }()
 
+    let mileLabel:UILabel = {
+        
+        let label = UILabel()
+        label.textColor     = UIColor.white
+        label.font          = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .right
+        return label
+    }()
+    
+    let typeLabel:UILabel = {
+        
+        let label = UILabel()
+        label.textColor     = UIColor.white
+        label.font          = UIFont.systemFont(ofSize: 15)
+        label.textAlignment = .left
+        return label
+    }()
+    
     let contentLabel:UILabel = {
 
         let label = UILabel()
-        label.textColor     = UIColor.black
+        label.textColor     = UIColor.white
         label.font          = UIFont.systemFont(ofSize: 18)
         return label
     }()
 
-    let vinLabel:UILabel = {
+    let sanLabel:UILabel = {
 
         let label = UILabel()
         label.textColor     = UIColor.white
-        label.font          = UIFont.systemFont(ofSize: 15)
+        label.font          = UIFont.systemFont(ofSize: 18)
         return label
     }()
 
     override func commonInit() {
-        self.backgroundColor = UIColor.clear
+        self.backgroundColor = UIColor(red: 47/255, green: 47/255, blue: 47/255, alpha: 1.00)
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        
+        bgView = BGView(frame: CGRect(
+            origin: CGPoint(x: 10, y: 10),
+            size: CGSize(width: screenWidth - 20, height: ReportTableViewCell.cellHeight() - 20)))
         self.contentView.addSubview(bgView)
 
-        self.vinLabel.title = "VIN:"
+        self.sanLabel.title = "操作人员:"
 
-        bgView.addSubview(titleLabel)
+        let typeNameLabel:UILabel = {
+            
+            let label = UILabel()
+            label.textColor     = UIColor.white
+            label.font          = UIFont.systemFont(ofSize: 18)
+            label.textAlignment = .left
+            return label
+        }()
+        typeNameLabel.text = "类型:";
+        
+        let descNameLabel:UILabel = {
+            
+            let label = UILabel()
+            label.textColor     = UIColor.white
+            label.font          = UIFont.systemFont(ofSize: 18)
+            label.textAlignment = .left
+            return label
+        }()
+        descNameLabel.text = "维修说明:";
+        
+        bgView.addSubview(typeNameLabel)
+        bgView.addSubview(descNameLabel)
+        bgView.addSubview(descLabel)
+        bgView.addSubview(typeLabel)
         bgView.addSubview(dateLabel)
-        bgView.addSubview(contentLabel)
-        bgView.addSubview(vinLabel)
+        bgView.addSubview(mileLabel)
+//        bgView.addSubview(contentLabel)
+        bgView.addSubview(sanLabel)
 
-        bgView.snp.makeConstraints { (make) in
-            make.size.equalTo(bgView.image!.size)
-            make.center.equalTo(self)
-        }
+//        bgView.snp.makeConstraints { (make) in
+//            make.size.equalTo(bgView.image!.size)
+//            make.center.equalTo(self)
+//        }
 
         let padding:CGFloat     = 20
         let labelHeight:CGFloat = 20
 
         let topPadding:CGFloat = 8
-        titleLabel.snp.makeConstraints { (make) in
+        
+        typeNameLabel.snp.makeConstraints { (make) in
             make.left.equalTo(bgView).offset(padding)
-            make.top.equalTo(bgView).offset(topPadding)
-            make.width.equalTo(bgView).multipliedBy(0.6)
+            make.top.equalTo(bgView).offset(40)
+            make.width.equalTo(bgView).multipliedBy(0.8)
             make.height.equalTo(labelHeight)
         }
 
+        typeLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(bgView).offset(padding)
+            make.top.equalTo(bgView).offset(70)
+            make.width.equalTo(bgView).multipliedBy(0.8)
+            make.height.equalTo(labelHeight)
+        }
+        
+        let line:CGFloat = 5;
+        descNameLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(bgView).offset(padding)
+            make.top.equalTo(bgView).offset(ReportTableViewCell.cellHeight() - padding - labelHeight * line - CGFloat(20.0))
+            make.width.equalTo(bgView).multipliedBy(0.8)
+            make.height.equalTo(labelHeight)
+        }
+        
+        descLabel.numberOfLines = 0
+        descLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(bgView).offset(padding)
+            make.top.equalTo(bgView).offset(ReportTableViewCell.cellHeight() - padding - labelHeight * line)
+            make.width.equalTo(bgView).multipliedBy(0.8)
+            make.height.equalTo(labelHeight * line)
+        }
 
         dateLabel.snp.makeConstraints { (make) in
-            make.right.equalTo(bgView).offset(-padding)
+            make.left.equalTo(bgView).offset(padding+50)
             make.top.equalTo(bgView).offset(topPadding)
             make.width.equalTo(bgView).multipliedBy(0.3)
             make.height.equalTo(labelHeight)
         }
 
-        contentLabel.snp.makeConstraints { (make) in
-            make.left.equalTo(bgView).offset(padding)
-            make.centerY.equalTo(bgView).offset(-7)
-            make.width.equalTo(bgView).multipliedBy(0.8)
+        mileLabel.snp.makeConstraints { (make) in
+            make.right.equalTo(bgView).offset(-padding)
+            make.top.equalTo(bgView).offset(topPadding)
+            make.width.equalTo(bgView).multipliedBy(0.3)
+            make.height.equalTo(labelHeight)
         }
+        
+//        contentLabel.snp.makeConstraints { (make) in
+//            make.left.equalTo(bgView).offset(padding)
+//            make.centerY.equalTo(bgView).offset(-7)
+//            make.width.equalTo(bgView).multipliedBy(0.8)
+//        }
 
-        vinLabel.snp.makeConstraints { (make) in
+        sanLabel.snp.makeConstraints { (make) in
             make.left.equalTo(bgView).offset(padding)
-            make.bottom.equalTo(bgView).offset(-22)
+            make.top.equalTo(bgView).offset(100)
             make.width.equalTo(bgView).multipliedBy(0.7)
         }
     }
 
     static func cellHeight() -> CGFloat {
-        return 140
+        return 280
     }
 }
